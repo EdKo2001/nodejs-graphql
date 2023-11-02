13e27465747b98d97017792ae223d9e2dd09191b
Eduard Koshkelyan <eduardko2001@gmail.com>
2023-11-03 00:08:51 +0400

    fix(authed-request): add functionality

--- dist/lib/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.env = void 0;
-var env_1 = require("./env");
-Object.defineProperty(exports, "env", { enumerable: true, get: function () { return __importDefault(env_1).default; } });
